aeea083234fc96ce85cee799133d7fc3ea95ab73
pixelport <johanngillhoff@yahoo.de>
2022-01-27 15:39:48 +0100

    do not use hardcoded DynamoDb config if jest is detected

--- src/ddb.ts
+++ src/ddb.ts
@@ -5,7 +5,7 @@ import {InsertManyParams} from './ddb.types';
 const isTest = process.env.JEST_WORKER_ID;
 const config = {
   convertEmptyValues: true,
-  ...(isTest && {endpoint: 'localhost:8000', sslEnabled: false, region: 'local-env'}),
+   // ...(isTest && {endpoint: 'localhost:8000', sslEnabled: false, region: 'local-env'}),
 };
 
 const documentClient: DocumentClient = new DynamoDB.DocumentClient(config);
